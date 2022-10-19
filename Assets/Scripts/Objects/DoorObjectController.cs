bb4c45f095dc9545334ad1d844f0aeb571176283
BiscuitPrime <henri.nomico@gmail.com>
2022-10-19 14:17:29 +0200

    Added basic Door and Lamp object scripts

---
+++ Assets/Scripts/Objects/DoorObjectController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  Component handling the door's modifications and state according to its file
+ *  Associated file : Door.txt
+ *  Values in file :
+ *      status : open/closed
+ */
+public class DoorObjectController : ModifiableController
+{
+    public override void UpdateModification()
+    {
+        //For the lamp object, we test if its power is on or off
+        if (properties.ContainsKey("status"))
+        {
+            if (properties["status"] == "open")
+            {
+                print("DOOR OPEN");
+                //CODE TO OPEN DOOR
+            }
+            else if (properties["status"] == "closed")
+            {
+                print("DOOR CLOSE");
+                //CODE TO CLOSE DOOR
+            }
+        }
+    }
+}
