9d5bc31f6e35af158a02c952e03ff2be3cd10780
BiscuitPrime <henri.nomico@gmail.com>
2022-11-15 21:19:27 +0100

    Fixed interaction&deletion weird interaction

--- Assets/Scripts/Player/PlayerInteractionController.cs
+++ Assets/Scripts/Player/PlayerInteractionController.cs
@@ -13,6 +13,10 @@ public class PlayerInteractionController : MonoBehaviour
     [SerializeField] private GameObject interactionPrompt;
     Grid grid;
 
+    //Variables used to save the last target and direction to use in defensive procedures to check if object still remains everytime the game comes back in focus
+    private Vector3 lastTarget;
+    private Vector2Int lastDirection;
+
 
     Vector3 debugValue;                                            
     public Interactable lastInteractable { get; private set; }
@@ -45,6 +49,10 @@ public class PlayerInteractionController : MonoBehaviour
      */
     public void IsInteracting(Vector3 target, Vector2Int direction) //look if player can interact with object or NPC
     {
+        //We save the direction and target to allow check for when window comes back in focus
+        lastDirection = direction;
+        lastTarget = target;
+
         Collider2D hit = Physics2D.OverlapBox(target+(Vector3Int) direction, grid.cellSize - Vector3.one * 0.1f, 0);
         debugValue = target + new Vector3(direction.x, 0, direction.y);
         if (hit)
@@ -66,8 +74,19 @@ public class PlayerInteractionController : MonoBehaviour
         }
     }
 
+    /**
+     *  Function called everytime the game comes in focus
+     *  Tests if the last detected object can be detected again, i.e. has not moved nor been deleted
+     */
+    private void OnApplicationFocus(bool focus)
+    {
+        if(lastTarget!=null && lastDirection != null)
+        {
+            IsInteracting(lastTarget, lastDirection);
+        }
+    }
 
-/*    void OnDrawGizmos()
+    /*void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(debugValue , grid.cellSize - Vector3.one * 0.1f);
