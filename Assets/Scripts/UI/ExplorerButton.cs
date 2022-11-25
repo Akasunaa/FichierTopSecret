e97975766bfe5cc1617db370f27a194d7afb2f9f
PtiBouchon <lopite2@live.fr>
2022-11-25 15:31:05 +0100

    Fix issues

---
+++ Assets/Scripts/UI/ExplorerButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplorerButton : MonoBehaviour
+{
+    public static void OpenExplorer()
+    {
+        Application.OpenURL("file:///" + Application.streamingAssetsPath + "/Test");
+    }
+}
