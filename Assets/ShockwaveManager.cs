573ffd1e77455b074695635ac9df21570245440b
Hugo <hugocarbiener@gmail.com>
2022-12-27 21:31:52 +0100

    Add shader and background to cosmic bin

---
+++ Assets/ShockwaveManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/**
+ * Used on the ground tilemap in the cosmic bin to make the shockwave follow the player
+ */
+public class ShockwaveManager : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private TilemapRenderer renderer;
+
+    private void Awake()
+    {
+        if (!renderer) renderer = GetComponent<TilemapRenderer>();
+    }
+
+    private void Update()
+    {
+        renderer.material.SetVector("_FocalPoint", new Vector4(player.position.x, player.position.y, 0, 0));
+    }
+}
