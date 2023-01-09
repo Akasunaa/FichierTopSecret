780a6c00119339c93729dd9eb1f895fe1117c135
PtiBouchon <lopite2@live.fr>
2023-01-09 16:55:07 +0100

    Add ruler script

---
+++ Assets/Scripts/UI/Ruler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruler : MonoBehaviour
+{
+    public void DrawRuler() // Reglette
+     {
+         GUI.Label(new Rect(0, 0, 100, 100), "HELLO");
+     }
+}
