343beabb6ede842d97dcb00912dfde6f0d593cc7
BiscuitPrime <henri.nomico@gmail.com>
2023-01-16 15:26:05 +0100

    Added special death dialog states that handle a better death than just depop

--- Assets/Scripts/Actors/NPC Dialogue System/DeathDialogState.cs
+++ Assets/Scripts/Actors/NPC Dialogue System/DeathDialogState.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class DeathDialogState : MonoBehaviour
+/**
+ *  Script used by special dialog states triggered when an NPC dies, that will, when ariving in their Exit() function, unactivate the the parent NPC
+ */
+[CreateAssetMenu(fileName = "Death Dialog State", menuName = "States/Death Dialog State")]
+public class DeathDialogState : DialogState
 {
-    // Start is called before the first frame update
-    void Start()
+    public DeathDialogState(string name, DialogSM SM) : base(name, SM)
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    /**
+     *  Inherited and overrided function that will handle the killing of the NPC
+     */
+    public override int ChangeSpeech()
     {
-        
+        interactionIndex++;
+        if (interactionIndex < speech.Length)
+        {
+            currentSpeech = speech[interactionIndex];
+            GetSpeechVariables(SM);
+            return 1;
+        }
+        else //when reaching the end of the various speeches, the NPC will die
+        {
+            currentSpeech = speech[^1];
+            GetSpeechVariables(SM);
+            TriggerNPCDeath();
+            return 0;
+        }
+    }
+
+    /**
+     *  Actual trigger of the NPC's death
+     */
+    public void TriggerNPCDeath()
+    {
+        SM.ConvertTo<DialogSM>().associatedNPCController.gameObject.SetActive(false);
     }
 }
