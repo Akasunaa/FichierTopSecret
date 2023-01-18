8aa35d9d97565d94976e129593d8a9b3b3c3923a
BiscuitPrime <henri.nomico@gmail.com>
2023-01-18 14:49:34 +0100

    Added ability to destroy walls behind ya

--- Assets/Scripts/System/ZoneDetectorController.cs
+++ Assets/Scripts/System/ZoneDetectorController.cs
@@ -7,11 +7,18 @@ using UnityEngine;
 /// </summary>
 public class ZoneDetectorController : MonoBehaviour
 {
+    [Header("Deactivable Elements upon detection")]
+    [SerializeField] private GameObject[] deactivableElements;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player") //when the player is detected, we will deactivate the relevant gameobjects
         {
             Debug.Log("PLAYER DETECTED");
+            foreach(var element in deactivableElements)
+            {
+                element.SetActive(false);
+            }
         }
     }
 }
