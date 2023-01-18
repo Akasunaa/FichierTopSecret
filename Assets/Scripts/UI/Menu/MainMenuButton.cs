e951637a6e7749986a5a36d700b9b90ef855b1ed
PtiBouchon <lopite2@live.fr>
2023-01-18 16:58:17 +0100

    Fix build bug with a UNUSED SHITTY IMPORT

--- Assets/Scripts/UI/Menu/MainMenuButton.cs
+++ Assets/Scripts/UI/Menu/MainMenuButton.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.PackageManager;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
