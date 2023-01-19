74374327debfd48f2449b60fa64e6cb624aa4ca4
BiscuitPrime <henri.nomico@gmail.com>
2023-01-19 10:45:32 +0100

    Added special factory guard death state that triggers door

---
+++ Assets/Scripts/Actors/NPC Dialogue System/FactoryGuardDeathState.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *      This script will be used to create a special state only used by the factory guard that will open a door when killed
+ */
+[CreateAssetMenu(fileName = "Factory Guard Death Dialog State", menuName = "States/Factory Guard Death Dialog State")]
+public class FactoryGuardDeathState : DeathDialogState
+{
+    [Header("PlayerPrefs Elements")]
+    [SerializeField] private string playerPrefsName;  //name of the player pref that we're going to modify upon call in the Enter() function
+    [SerializeField] private string playerPrefsValue; //value inputted in said playerpref
+    [Header("Factory Door Elements")]
+    [SerializeField] private string factoryDoorTag = "FactoryDoor";
+    private GameObject factoryDoor;
+
+    public FactoryGuardDeathState(string name, DialogSM SM) : base(name, SM)
+    {
+    }
+
+    /**
+     * Function Enter() called when state is loaded
+     * main difference given by that script : 
+     * here, unlike regular dialog states, we're going to modify a player pref to a certain value AND also open the associated door
+    **/
+    public override void Enter(StateMachine sm)
+    {
+        base.Enter(sm);
+        factoryDoor = GameObject.FindGameObjectWithTag(factoryDoorTag);
+        if (factoryDoor != null)
+        {
+            factoryDoor.GetComponent<FactoryDoorController>().UnlockDoor();
+        }
+        if (PlayerPrefs.GetString(playerPrefsName) != "READ") //if the value has already been read by the recepient, we do not change it anymore
+        {
+            PlayerPrefs.SetString(playerPrefsName, playerPrefsValue); //we write the wanted value
+            PlayerPrefs.Save();                                       //we save it
+        }
+    }
+
+}
