6457cde61589d82b6edcca47653e2602c543a8b8
ElRaphik <raphael.soranzo@gmail.com>
2023-01-20 15:26:14 +0100

    removed useless imports

--- Assets/Scripts/UI/GameOverScreen/GameOverScreenController.cs
+++ Assets/Scripts/UI/GameOverScreen/GameOverScreenController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 
 
 public class GameOverScreenController : MonoBehaviour
