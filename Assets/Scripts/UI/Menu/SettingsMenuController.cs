d43c7dfbc211414033410a42f63637847063a404
ElRaphik <raphael.soranzo@gmail.com>
2023-01-23 17:26:21 +0100

    on peut changer la résolution chef (et le taux de rafraichissement)

--- Assets/Scripts/UI/Menu/SettingsMenuController.cs
+++ Assets/Scripts/UI/Menu/SettingsMenuController.cs
@@ -1,12 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class SettingsMenuController : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenuCanvas;
+
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown refreshRateDropdown;
+
+    private readonly List<Resolution> _resolutions = new();
     
+    private readonly struct ScreenRatio
+    {
+        private readonly int _widthFactor;
+        private readonly int _heightFactor;
+
+        public ScreenRatio(int wFactor, int hFactor)
+        {
+            _widthFactor = wFactor;
+            _heightFactor = hFactor;
+        }
+
+        public int WidthFromHeight(int height)
+        {
+            return height * _widthFactor / _heightFactor;
+        }
+
+        public int HeightFromWidth(int width)
+        {
+            return width * _heightFactor / _widthFactor;
+        }
+    }
+
+    private readonly int[] _acceptedScreenWidths = { 800, 1000, 1600 };
+    private readonly List<int> _acceptedRefreshRates = new() { 30, 60, 75, 120, 144, 240 };
+    
+    private readonly ScreenRatio _screenRatio = new(4, 3);
+
+    private int _currentRefreshRateIndex = -1;
+    private int _currentResolutionIndex;
+
+    private void Start()
+    {
+        BuildResolutions();
+        
+        SetRefreshRateIndex();
+        
+        SetRefreshRateDropdown();
+        
+        SetResolutionDropdown();
+    }
+
+    #region Starting methods
+    
+    private void BuildResolutions()
+    {
+        foreach (var width in _acceptedScreenWidths)
+        {
+            _resolutions.Add(new Resolution
+            {
+                width = width,
+                height = _screenRatio.HeightFromWidth(width),
+                refreshRate = 30 // dummy refresh rate
+            });
+        }
+    }
+    private void SetRefreshRateIndex()
+    {        
+        for (var i = 0; i < _acceptedRefreshRates.Count; i++) 
+            if (_acceptedRefreshRates[i] == Screen.currentResolution.refreshRate) 
+                _currentRefreshRateIndex = i;
+
+        if (_currentRefreshRateIndex >= 0) return;
+        
+        _currentRefreshRateIndex = _acceptedRefreshRates.Count;
+        _acceptedRefreshRates.Add(Screen.currentResolution.refreshRate);
+    }
+
+    private void SetRefreshRateDropdown()
+    {
+        var options = _acceptedRefreshRates.Select(rate => rate + "Hz").ToList();
+        
+        refreshRateDropdown.ClearOptions();
+        refreshRateDropdown.AddOptions(options);
+        refreshRateDropdown.value = _currentRefreshRateIndex;
+        refreshRateDropdown.RefreshShownValue();
+    }
+    
+    private void SetResolutionDropdown()
+    {
+        List<string> options = new();
+        for (var i = 0; i < _resolutions.Count; i++)
+        {
+            var resolution = _resolutions[i];
+            var resolutionOption = resolution.width + "x" + resolution.height;
+            options.Add(resolutionOption);
+            if (resolution.width == Screen.width && resolution.height == Screen.height)
+                _currentResolutionIndex = i;
+        }
+        
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = _currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    
+    #endregion
+
+
+    #region Called by UI methods
+
+    public void SetResolution(int resolutionIndex)
+    {
+        var resolution = _resolutions[resolutionIndex];
+        _currentResolutionIndex = resolutionIndex;
+        Screen.SetResolution(resolution.width, resolution.height, false);
+    }
+
+    public void SetRefreshRate(int refreshRateIndex)
+    {
+        var rate = _acceptedRefreshRates[refreshRateIndex];
+        _currentRefreshRateIndex = refreshRateIndex;
+        Application.targetFrameRate = rate;
+    }
+
     public void ToMainMenu()
     {
         mainMenuCanvas.SetActive(true);
         gameObject.SetActive(false);
     }
+    
+    #endregion
 }
