d6dfa55de42b3fe0bd7f52878dbd52d379adccb7
BiscuitPrime <henri.nomico@gmail.com>
2023-01-25 11:29:59 +0100

    Added cinematic factory room 2

--- Assets/Scripts/System/ZoneDetectorController.cs
+++ Assets/Scripts/System/ZoneDetectorController.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 /// <summary>
-/// Class used to handle zones of detection that, if the player passes through, save a data in PlayerPrefs
+/// Class used to handle zones of detection that, if the player passes through, acctivates a certain cinematic
 /// </summary>
 public class ZoneDetectorController : MonoBehaviour
 {
-    [Header("Deactivable Elements upon detection")]
-    [SerializeField] private GameObject[] deactivableElements;
+    [Header("Cinematic element to activate upon detection")]
+    [SerializeField] private GameObject activableElement;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player") //when the player is detected, we will deactivate the relevant gameobjects
         {
-            Debug.Log("PLAYER DETECTED");
-            foreach(var element in deactivableElements)
-            {
-                element.SetActive(false);
-            }
+            PlayerPresenceTriggeredFunction();
         }
+    }
+
+    private void PlayerPresenceTriggeredFunction()
+    {
+        Debug.Log("PLAYER DETECTED");
+        activableElement.SetActive(true);
     }
 }
