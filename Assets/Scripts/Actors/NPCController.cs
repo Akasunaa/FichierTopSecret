2adb8913944b70bc4f4ffb005203606b05128d8e
BiscuitPrime <henri.nomico@gmail.com>
2023-01-27 16:04:03 +0100

    fixed npc SetDefaultProperties issue

--- Assets/Scripts/Actors/NPCController.cs
+++ Assets/Scripts/Actors/NPCController.cs
@@ -82,64 +82,65 @@ public class NPCController : ModifiableController, Interactable
         ui = GameObject.FindGameObjectsWithTag("UI")[0].GetComponent<DialogueUIController>();
         Assert.IsNotNull(ui);
 
-        //Creating the dict of the values :
-        foreach(var element in fileProperties)
-        {
-            if (!propertyDict.ContainsKey(element.propertyName))
-            {
-                propertyDict.Add(element.propertyName, element);
-            }
-        }
-        //-----------------------------------
+        ////Creating the dict of the values :
+        //foreach(var element in fileProperties)
+        //{
+        //    if (!propertyDict.ContainsKey(element.propertyName))
+        //    {
+        //        propertyDict.Add(element.propertyName, element);
+        //    }
+        //}
+        ////-----------------------------------
 
-        //Creating the dict of the objects :
-        foreach (var element in objectsElements)
-        {
-            if (!objectDict.ContainsKey(element.playerItemName))
-            {
-                objectDict.Add(element.playerItemName, element);
-            }
-        }
+        ////Creating the dict of the objects :
+        //foreach (var element in objectsElements)
+        //{
+        //    if (!objectDict.ContainsKey(element.playerItemName))
+        //    {
+        //        objectDict.Add(element.playerItemName, element);
+        //    }
+        //}
+        ////-----------------------------------
 
-        //Creating the dict of the quest items to give out :
-        foreach (var element in questItems)
-        {
-            if (!questItemsDict.ContainsKey(element.questChangeState))
-            {
-                questItemsDict.Add(element.questChangeState, element);
-            }
-        }
-        //-----------------------------------
+        ////Creating the dict of the quest items to give out :
+        //foreach (var element in questItems)
+        //{
+        //    if (!questItemsDict.ContainsKey(element.questChangeState))
+        //    {
+        //        questItemsDict.Add(element.questChangeState, element);
+        //    }
+        //}
+        ////-----------------------------------
 
-        //Creating the dict of the react elements to check out for :
-        foreach (var element in reactElements)
-        {
-            if (!reactElementsDict.ContainsKey(element.tagToReact))
-            {
-                reactElementsDict.Add(element.tagToReact, element);
-            }
-        }
-        //-----------------------------------
+        ////Creating the dict of the react elements to check out for :
+        //foreach (var element in reactElements)
+        //{
+        //    if (!reactElementsDict.ContainsKey(element.tagToReact))
+        //    {
+        //        reactElementsDict.Add(element.tagToReact, element);
+        //    }
+        //}
+        ////-----------------------------------
 
-        //Creating the dict of the player.txt elements to check out for :
-        foreach (var element in playerProperties)
-        {
-            if (!playerPropertiesDict.ContainsKey(element.playerPropertyName))
-            {
-                playerPropertiesDict.Add(element.playerPropertyName, element);
-            }
-        }
-        //-----------------------------------
+        ////Creating the dict of the player.txt elements to check out for :
+        //foreach (var element in playerProperties)
+        //{
+        //    if (!playerPropertiesDict.ContainsKey(element.playerPropertyName))
+        //    {
+        //        playerPropertiesDict.Add(element.playerPropertyName, element);
+        //    }
+        //}
+        ////-----------------------------------
 
-        //Creating the dict of the Player Prefs Elements to check out for :
-        foreach (var element in playerPrefsElements)
-        {
-            if (!playerPrefsElementsDict.ContainsKey(element.playerPrefsName))
-            {
-                playerPrefsElementsDict.Add(element.playerPrefsName, element);
-            }
-        }
-        //-----------------------------------
+        ////Creating the dict of the Player Prefs Elements to check out for :
+        //foreach (var element in playerPrefsElements)
+        //{
+        //    if (!playerPrefsElementsDict.ContainsKey(element.playerPrefsName))
+        //    {
+        //        playerPrefsElementsDict.Add(element.playerPrefsName, element);
+        //    }
+        //}
+        ////-----------------------------------
 
         //we set up the different variables of the NPC controller that do not require external help :
         player = GameObject.FindGameObjectWithTag("Player");
@@ -377,6 +378,66 @@ public class NPCController : ModifiableController, Interactable
     /// </summary>
     public override void SetDefaultProperties()
     {
+        //Creating the dict of the values :
+        foreach (var element in fileProperties)
+        {
+            if (!propertyDict.ContainsKey(element.propertyName))
+            {
+                propertyDict.Add(element.propertyName, element);
+            }
+        }
+        //-----------------------------------
+
+        //Creating the dict of the objects :
+        foreach (var element in objectsElements)
+        {
+            if (!objectDict.ContainsKey(element.playerItemName))
+            {
+                objectDict.Add(element.playerItemName, element);
+            }
+        }
+        //-----------------------------------
+
+        //Creating the dict of the quest items to give out :
+        foreach (var element in questItems)
+        {
+            if (!questItemsDict.ContainsKey(element.questChangeState))
+            {
+                questItemsDict.Add(element.questChangeState, element);
+            }
+        }
+        //-----------------------------------
+
+        //Creating the dict of the react elements to check out for :
+        foreach (var element in reactElements)
+        {
+            if (!reactElementsDict.ContainsKey(element.tagToReact))
+            {
+                reactElementsDict.Add(element.tagToReact, element);
+            }
+        }
+        //-----------------------------------
+
+        //Creating the dict of the player.txt elements to check out for :
+        foreach (var element in playerProperties)
+        {
+            if (!playerPropertiesDict.ContainsKey(element.playerPropertyName))
+            {
+                playerPropertiesDict.Add(element.playerPropertyName, element);
+            }
+        }
+        //-----------------------------------
+
+        //Creating the dict of the Player Prefs Elements to check out for :
+        foreach (var element in playerPrefsElements)
+        {
+            if (!playerPrefsElementsDict.ContainsKey(element.playerPrefsName))
+            {
+                playerPrefsElementsDict.Add(element.playerPrefsName, element);
+            }
+        }
+        //-----------------------------------
+
         foreach (var propertyKey in propertyDict.Keys)
         {
             // as they are default properties, they are considered as important
